48d7e1734c77dd6e4ba7bbec681c1ff53aeec11f
Jordan Hudgens <jordan@devcamp.com>
2017-06-22 14:45:14 +0000

    Integrated new and create actions for portfolios

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,9 +31,9 @@ puts "10 blog posts created"
   
   puts "5 skills created"
   
-  9.times do |portfolio_items|
+  9.times do |portfolio_item|
     Portfolio.create!(
-      title: "Portfolio title: #{portfolio_items}", 
+      title: "Portfolio title: #{portfolio_item}", 
       subtitle: "My great service",
       body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
       magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
