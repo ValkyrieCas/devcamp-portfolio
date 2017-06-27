95af1d6af619debda2367b0028e386f90d38b45b
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 17:55:33 +0000

    Implemented custom scopes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+3.times do |topic|
+ Topic.create!(
+ title: "Topic #{topic}"
+ ) 
+end
+
+puts "3 topics created"
 
 10.times do |blog|
  Blog.create!(
@@ -16,7 +23,8 @@
    adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. 
    Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi 
    consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel 
-   illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+   illum qui dolorem eum fugiat quo voluptas nulla pariatur?",
+   topic_id: Topic.last.id
    )
 end 
 
@@ -31,10 +39,10 @@ puts "10 blog posts created"
   
   puts "5 skills created"
   
-  9.times do |portfolio_item|
+  8.times do |portfolio_item|
     Portfolio.create!(
       title: "Portfolio title: #{portfolio_item}", 
-      subtitle: "My great service",
+      subtitle: "Ruby on Rails",
       body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
       magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
       Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
@@ -43,5 +51,19 @@ puts "10 blog posts created"
       thumb_image: "http://via.placeholder.com/350x200"
      )
   end 
+  
+   1.times do |portfolio_item|
+    Portfolio.create!(
+      title: "Portfolio title: #{portfolio_item}", 
+      subtitle: "Angular",
+      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      main_image: "http://via.placeholder.com/600x400", 
+      thumb_image: "http://via.placeholder.com/350x200"
+     )
+  end 
+  
   
   puts "9 portfolio items created"
